7dc14f765027d017287d18ed0045c4cc4c60e4ce
y2k <itwisterlx@gmail.com>
2020-02-12 13:21:32 +0300

    refactoring

--- Program.fs
+++ Program.fs
@@ -2,100 +2,114 @@
 
 [<AutoOpen>]
 module Prelude =
+    let inline flip f a b = f b a
+    let inline always x _ = x
     let inline (^) f x = f x
     let inline (>>-) a f = async {
         let! x = a
         return f x }
 
-module TelegramConnector =
-    open TLSharp.Core
-    open TLSharp.Core.Network
+module Domain = 
     open TeleSharp.TL
-    open TeleSharp.TL.Contacts
     open TeleSharp.TL.Messages
-
-    module L =
-        let log = printfn "LOG :: %s"
 
     type Snapshot =
         { title : string
           uri : Uri }
 
+    let parse (history : TLAbsMessages) chatName =
+        (history :?> TLChannelMessages).Messages
+        |> Seq.choose ^ function | :? TLMessage as x -> Some x | _ -> None
+        |> Seq.map ^ fun x ->
+            { title = x.Message
+              uri = Uri <| sprintf "https://t.me/%s/%i" chatName x.Id }
+        |> Seq.rev
+        |> Seq.toList
+
+type Global =
+    { client : unit -> TLSharp.Core.TelegramClient }
+    with 
+        static member instance = ref { client = fun _ -> failwith "???" }
+        static member update (f : Global -> Global) =
+            Global.instance := f !Global.instance
+        static member client' = (!Global.instance).client()
+
+module TelegramConnector =
+    open TLSharp.Core
+    open TeleSharp.TL
+    open TeleSharp.TL.Contacts
+
+    let private log = printfn "LOG :: %s"
+
     let mkClient appId apiHash phone = async {
         let client = new TelegramClient(appId, apiHash)
         if client.IsUserAuthorized() then
-            L.log "Telegram authorized"
+            log "Telegram authorized"
             return client, ""
         else
             let! hash = client.SendCodeRequestAsync phone |> Async.AwaitTask
-            L.log ^ sprintf "Telegram required code (hash = %s)" hash
+            log ^ sprintf "Telegram required code (hash = %s)" hash
             return client, hash }
 
-    let updateToken (client : TelegramClient) phone hash code = async {
-        L.log ^ sprintf "Telegram setCode called, code = %s" code
-        let! r = client.MakeAuthAsync(phone, hash, code) |> Async.AwaitTask |> Async.Catch
-        L.log ^ sprintf "Telegram code applied, result = %O" r }
+    let updateToken phone hash code = async {
+        log ^ sprintf "Telegram setCode called, code = %s" code
+        let! r = Global.client'.MakeAuthAsync(phone, hash, code) |> Async.AwaitTask |> Async.Catch
+        log ^ sprintf "Telegram code applied, result = %O" r }
 
-    let getNodes (client : TelegramClient) (uri : Uri) = async {
+    let getNodes (uri : Uri) = async {
         let chatName = uri.Segments.[1]
-        let r = TLRequestResolveUsername()
-        r.Username <- chatName
-        let! response = client.SendRequestAsync r |> Async.AwaitTask
+        let! response =
+            TLRequestResolveUsername(Username = chatName)
+            |> Global.client'.SendRequestAsync
+            |> Async.AwaitTask
         let channel = (response :> TLResolvedPeer).Chats.[0] :?> TLChannel
-        L.log ^ sprintf "Response = %O | id = %O" channel.Username channel.Id
-        let i = TLInputPeerChannel()
-        i.ChannelId <- channel.Id
-        i.AccessHash <- channel.AccessHash.Value
-        let! history = client.GetHistoryAsync(i, 0, 0, 50) |> Async.AwaitTask
-        return
-            (history :?> TLChannelMessages).Messages
-            |> Seq.choose ^ function | :? TLMessage as x -> Some x | _ -> None
-            |> Seq.map ^
-                fun x ->
-                    { title = x.Message
-                      uri = Uri <| sprintf "https://t.me/%s/%i" chatName x.Id }
-            |> Seq.rev
-            |> Seq.toList }
-
-type Global =
-    { client : TLSharp.Core.TelegramClient }
-    with 
-        static member instance () : Global = failwith "???"
-        static member set (_ : Global) = () // FIXME:
+        log ^ sprintf "Response = %O | id = %O" channel.Username channel.Id
+        let i = TLInputPeerChannel(ChannelId = channel.Id, AccessHash = channel.AccessHash.Value)
+        let! history = Global.client'.GetHistoryAsync(i, 0, 0, 50) |> Async.AwaitTask
+        return Domain.parse history }
 
 module Server =
     open Suave
     open Suave.Filters
     open Suave.Operators
+    open Suave.Form
     module P = TelegramConnector
 
     let simleRead (uri : string) ctx = async {
-        let! r =
+        let! result =
             Uri uri
-            |> P.getNodes (Global.instance().client)
+            |> P.getNodes
             >>- (Json.toJson >> Successful.ok)
-        return! r ctx }
+        return! result ctx }
 
     let reset = request ^ fun r ->
-        match r.formData "appId", r.formData "apiHash", r.formData "phone" with
-        | Choice1Of2 appId, Choice1Of2 apiHash, Choice1Of2 phone ->
+        let formDesc : Form<{| appId : string; apiHash : string; phone : string |}> =
+            Form ([ TextProp ((fun f -> <@ f.appId @>), [ maxLength 256 ])
+                    TextProp ((fun f -> <@ f.apiHash @>), [ maxLength 256 ])
+                    TextProp ((fun f -> <@ f.phone @>), [ maxLength 256 ]) ], [])
+        bindForm formDesc r 
+        |> Choice.map ^ fun x -> 
             fun ctx -> async {
-                let! (client, hash) = P.mkClient (int appId) apiHash phone
-                Global.set { client = client }
+                let! (client, hash) = P.mkClient (int x.appId) x.apiHash x.phone
+                Global.update ^ fun g -> { g with client = always client }
                 return! Successful.OK hash ctx }
-        | _ -> RequestErrors.BAD_REQUEST ""
+        |> Choice.fold id RequestErrors.BAD_REQUEST
 
     let setCode = request ^ fun r ->
-        match r.formData "phone", r.formData "hash", r.formData "code" with
-        | Choice1Of2 phone, Choice1Of2 hash, Choice1Of2 code ->
+        let formDesc : Form<{| hash : string; code : string; phone : string |}> =
+            Form ([ TextProp ((fun f -> <@ f.hash @>), [ maxLength 256 ])
+                    TextProp ((fun f -> <@ f.code @>), [ maxLength 256 ])
+                    TextProp ((fun f -> <@ f.phone @>), [ maxLength 256 ]) ], [])
+        bindForm formDesc r 
+        |> Choice.map ^ fun x -> 
             fun ctx -> async {
-                do! P.updateToken (Global.instance().client) phone hash code
+                do! P.updateToken x.phone x.hash x.code
                 return! Successful.NO_CONTENT ctx }
-        | _ -> RequestErrors.BAD_REQUEST ""
+        |> Choice.fold id RequestErrors.BAD_REQUEST
 
     let start pass =
         choose [
-            GET >=> pathScan "/simple-read/%s" simleRead
+            POST >=> pathScan "/simple-read/%s" simleRead
             Authentication.authenticateBasic
                 (fun (_, p) -> p = pass)
                 ^ choose [
