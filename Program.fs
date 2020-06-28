9586485b4cacdf84e63505f96c47daf5302527b8
y2k <itwisterlx@gmail.com>
2020-06-29 00:30:16 +0300

    add runtime cache

--- Program.fs
+++ Program.fs
@@ -7,13 +7,15 @@ module Telegram =
     open TeleSharp.TL
     open TeleSharp.TL.Contacts
 
+    type History = {| messages : {| created : int; fromId : int option; id : int; message : string |} list; users : {| firstName : string; lastName : string; id : int |} list |}
+
     type Msg = 
         | ResetClient of appId : int * apiHash : string
         | Connect of reply : AsyncReplyChannel<bool>
         | SendCode of phone : string
         | MakeAuth of code : string
         | ResolveUsername of name : string * AsyncReplyChannel<(int * int64 option) option>
-        | GetHistory of limit : int * id : (int * int64 option) * AsyncReplyChannel<{| messages : {| created : int; fromId : int option; id : int; message : string |} list; users : {| firstName : string; lastName : string; id : int |} list |}>
+        | GetHistory of limit : int * id : (int * int64 option) * AsyncReplyChannel<History>
 
     let private resolveUsername (client : TelegramClient) name =
         let toInputPeerChannel (response : TLResolvedPeer) =
@@ -59,7 +61,6 @@ module Telegram =
           mutable appId : int
           mutable apiHash : string
           mutable getHistoryCount : int64
-          mutable userIdCache : Map<String, (int * int64 option) option>
           mutable recreateCount : int64
           dir : IO.DirectoryInfo }
 
@@ -87,18 +88,13 @@ module Telegram =
             | MakeAuth code ->
                 do! state.client.MakeAuthAsync (state.phone, state.hash, code)
             | ResolveUsername (name, reply) ->
-                match Map.tryFind name state.userIdCache with
-                | Some id -> reply.Reply id
-                | None ->
-                    match! resolveUsername state.client name |> Async.Catch with
-                    | Choice1Of2 id ->
-                        state.userIdCache <- Map.add name id state.userIdCache
-                        reply.Reply id
-                    | Choice2Of2 e ->
-                        do! recreateClient e state
-                        let! id = resolveUsername state.client name
-                        state.userIdCache <- Map.add name id state.userIdCache
-                        reply.Reply id
+                match! resolveUsername state.client name |> Async.Catch with
+                | Choice1Of2 id ->
+                    reply.Reply id
+                | Choice2Of2 e ->
+                    do! recreateClient e state
+                    let! id = resolveUsername state.client name
+                    reply.Reply id
             | GetHistory (limit, (id, accessHash), reply) ->
                 state.getHistoryCount <- state.getHistoryCount + 1L
                 printfn "%O :: GetHistory (count = %O, IsConnected = %b)" DateTime.Now state.getHistoryCount state.client.IsConnected
@@ -116,13 +112,13 @@ module Telegram =
         MailboxProcessor.Start(
             fun inbox ->
                 async {
-                    let state = { client  = null; phone = ""; hash = ""; appId = 0; apiHash = ""; getHistoryCount = 0L; userIdCache = Map.empty; recreateCount = 0L; dir = dir }
+                    let state = { client  = null; phone = ""; hash = ""; appId = 0; apiHash = ""; getHistoryCount = 0L; recreateCount = 0L; dir = dir }
                     while true do
                         let! msg = inbox.Receive()
                         try 
-                            printfn "LOG IN = %O" msg
+                            printfn "%O :: LOG-IN = %O" DateTime.Now msg
                             do! handleMessage msg state
-                            printfn "LOG OUT = %O" msg
+                            printfn "%O :: LOG-OUT = %O" DateTime.Now msg
                         with e -> eprintfn "%O :: ERROR (%O): %O" DateTime.Now msg e
                 })
 
@@ -176,6 +172,18 @@ module ResponseMessages =
         |> Domain.toSnapshots
         |> System.Text.Json.JsonSerializer.Serialize
 
+module Cache =
+    open System.Runtime.Caching
+    type 't t = { cache : MemoryCache; livetime : TimeSpan }
+    type 't entry = { item : 't }
+    let create livetime = { cache = new MemoryCache ("cache"); livetime = livetime }
+    let get (t : 't t) key : 't option =
+        let r = t.cache.Get key
+        if isNull r then None
+        else (r :?> entry<'t>).item |> Some
+    let set (t : 't t) key (value : 't) =
+        t.cache.Set (key, { item = value}, DateTimeOffset.op_Implicit (DateTime.Now + t.livetime))
+
 module Service =
     open Telegram
 
@@ -195,12 +203,31 @@ module Service =
             return ResponseMessages.successAuthorized
         }
     let getLastMessages (telegram : MailboxProcessor<Msg>) r =
+        let userIdCache : Cache.t<(int * int64 option) option> = Cache.create <| TimeSpan.FromHours 1.
+        let historyCache : string Cache.t = Cache.create <| TimeSpan.FromMinutes 2.
         async {
             let username = Domain.parseString "chat" 32 r |> Result.unwrap
-            let! response = telegram.PostAndAsyncReply (fun r -> ResolveUsername (username, r))
+            let! response = 
+                match Cache.get userIdCache username with
+                | Some id -> 
+                    printfn "%O :: get username from cache for %s" DateTime.Now username
+                    async.Return id
+                | None ->
+                    async {
+                        let! r = telegram.PostAndAsyncReply (fun r -> ResolveUsername (username, r))
+                        Cache.set userIdCache username r
+                        return r
+                    }
             let chatId = response |> Result.ofOption (ResponseMessages.cantFindChat response) |> Result.unwrap 
-            let! history = telegram.PostAndAsyncReply (fun r -> GetHistory (25, chatId, r))
-            return history |> ResponseMessages.toJsonResponse
+            match Cache.get historyCache username with
+            | Some history -> 
+                printfn "%O :: return history from cache for %s" DateTime.Now username
+                return history
+            | None ->
+                let! history = telegram.PostAndAsyncReply (fun r -> GetHistory (25, chatId, r))
+                let response = history |> ResponseMessages.toJsonResponse
+                Cache.set historyCache username response
+                return response
         }
 
 module Server =
@@ -210,6 +237,7 @@ module Server =
 
     let start password =
         let telegram = Telegram.create "__data"
+        let getLastMessages = Service.getLastMessages telegram
         let runCommand g f = 
             request (fun r ctx -> 
                 async {
@@ -220,7 +248,7 @@ module Server =
         Authentication.authenticateBasic (fun (_, p) -> p = password) ^ choose [
             POST >=> path "/reset" >=> runCommand (fun r -> r.form) (Service.resetClient telegram)
             POST >=> path "/set-code" >=> runCommand (fun r -> r.form) (Service.login telegram)
-            GET >=> path "/history" >=> runCommand (fun r -> r.query) (Service.getLastMessages telegram) ]
+            GET >=> path "/history" >=> runCommand (fun r -> r.query) getLastMessages ]
         |> startWebServerAsync { defaultConfig with bindings = [ HttpBinding.createSimple HTTP "0.0.0.0" 8080 ] }
         |> snd
 
